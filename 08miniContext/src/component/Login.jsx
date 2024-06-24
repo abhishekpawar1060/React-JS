7622728c53d1cda998302e181eafa6ace47d4aaf
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-06-24 20:02:52 +0530

    Context API in react

---
+++ 08miniContext/src/component/Login.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Login(){
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const {setUser} = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+        //Prevent the page from refreshing
+        e.preventDefault()
+        setUser({username, password})
+    }
+
+    return(
+        <div>
+            <h1>Login</h1>
+            <input 
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="UserName"
+            />
+            {" "}
+            <input 
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+
+            <button 
+                onClick ={handleSubmit}
+            >Submit</button>
+        </div>
+    )
+}
+
+export default Login
---
+++ 08miniContext/src/component/Profile.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react"
+import UserContext from "../context/UserContext"
+
+function Profile(){
+    
+    const {user} = useContext(UserContext)
+
+    if(!user) return <h1>Not Logged In</h1>
+    return(
+        <>
+            <h1>Profile: {user.username}</h1>
+            <h1>More Component</h1>
+        </>
+    )
+}
+
+export default Profile
